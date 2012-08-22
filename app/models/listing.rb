91d64990a3357e6e0c14696434e3abf9ccd748ce
Kori Roys <kori@koriroys.com>
2012-08-22 14:51:56 -0500

    beware duplicate form fields

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
   attr_accessible :address, :price
+
+  has_many :photos
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessible :file, :listing_id, :title
 
+  belongs_to :listing
+
   mount_uploader :file, FileUploader
 end
