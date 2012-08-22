ebed6b6e13555230f3263f8eab48ca93326e05f4
Kori Roys <kori@koriroys.com>
2012-08-22 14:13:11 -0500

    blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveDemo::Application.config.secret_token = '0faa0cdfe3824f112fa8a0c7c1742e875533289665fc1166657afc836f5eceb5d0f7a24181845dd589df0f8cf22b5c6d09189873ff58011baf4cee1cd93ee481'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+CarrierwaveDemo::Application.routes.draw do
+  # root :to => 'welcome#index'
+end
